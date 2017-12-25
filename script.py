18381d67e06e283f27aeb5bd2014fa0590bbbdc8
Lucas <tavo92@gmail.com>
2017-12-25 20:59:23 -0300

    Cambios que hice para leer de configuracion, hay que testearlo bien

--- script.py
+++ script.py
@@ -1,7 +1,32 @@
 from run_test_epa import RunTestEPA
 from make_report_resume import make_report_resume
+import configparser
 
-JUNIT_JAR = '/usr/share/java/junit4-4.12.jar'
+
+def read_config_file(config_file):
+    config = configparser.ConfigParser()
+    config.read(config_file)
+
+    # Leo primero los valores de configuracion que se usan en todas las corridas
+    junit_jar = config['DEFAULT']['JUnitJAR']
+    evosuite_classes = config['DEFAULT']['EvoSuiteClasses']
+    evosuite_jar_path = config['DEFAULT']['EvoSuiteJARPath']
+
+    # Voy recorriendo lo que este definido
+    tests_to_run = []
+    runid = 0
+    for section in config.sections():
+        name = config[section]['Name']
+        code_dir = config[section]['CodeDir']
+        instrumented_code_dir = config[section]['InstrumentedCodeDir']
+        original_code_dir = config[section]['OriginalCodeDir']
+        class_name = config[section]['ClassName']
+        epa_path = config[section]['EPAPath']
+        criterion = config[section]['Criterion']
+        search_budget = config[section]['SearchBudget']
+        tests_to_run.append(RunTestEPA(name=name, junit_jar=junit_jar, code_dir=code_dir, instrumented_code_dir=instrumented_code_dir, original_code_dir=original_code_dir, evosuite_classes=evosuite_classes, evosuite_jar_path=evosuite_jar_path, class_name=class_name, epa_path=epa_path, criterion=criterion, search_budget=search_budget, runid=runid))
+        runid += 1
+    return tests_to_run
 
 if __name__ == '__main__':
     # Crear la clase y llamarla
@@ -25,3 +50,14 @@ if __name__ == '__main__':
     # Espero que terminen
     test_original.join()
     test_instrumented.join()
+
+    # Corro todos los tests
+    tests_to_run = read_config_file('config.txt')
+    chunk_size = 4
+    test_chunks = [tests_to_run[x:x+chunk_size] for x in range(0, len(tests_to_run), chunk_size)]
+    for chunk in test_chunks:
+        for test in chunk:
+            test.start()
+        for test in chunk:
+            test.join()
+    
