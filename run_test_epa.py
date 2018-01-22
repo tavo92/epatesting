203ebe13c7143533717188adcac2bbd7b9795920
Lucas <tavo92@gmail.com>
2018-01-22 13:48:37 -0300

    TODO cleared

--- run_test_epa.py
+++ run_test_epa.py
@@ -60,14 +60,6 @@ def run_pitest(workdir):
     subprocess.run(command, cwd=workdir, shell=True)
 
 def compile_workdir(workdir, evosuite_classes, output_directory):
-    ''' TODO:
-    -d directory
-Set the destination directory for class files. The directory must already exist; javac will not create it. If a class is part of a package, javac puts the class file in a subdirectory reflecting the package name, creating directories as needed. For example, if you specify -d C:\myclasses and the class is called com.mypackage.MyClass, then the class file is called C:\myclasses\com\mypackage\MyClass.class.
-If -d is not specified, javac puts each class files in the same directory as the source file from which it was generated.
-
-Note: The directory specified by -d is not automatically added to your user class path.
-
-    '''
     command_find = "find . -name '*.java' > sources.txt"
     print_command(command_find, workdir)
     subprocess.run(command_find, cwd=workdir, shell=True)
