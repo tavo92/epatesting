f7201b8d021b1e5d231087356d4b78486d38f694
j-godoy <j.godoy277@gmail.com>
2018-05-17 20:16:44 -0300

    dont remove results folder

--- run_epatesting.sh
+++ run_epatesting.sh
@@ -5,6 +5,6 @@ echo "Iniciando Epa Testing..."
 cd /home/jgodoy/workspace-epa/epatesting
 newName=$(date +%Y-%m-%d_%H%M%S)
 newName=epa_output.$newName
-rm -r /home/jgodoy/workspace-epa/epatesting/results/ /home/jgodoy/workspace-epa/epatesting/evosuite-report/ /home/jgodoy/workspace-epa/epatesting/all_resumes.csv
+rm -r /home/jgodoy/workspace-epa/epatesting/evosuite-report/ /home/jgodoy/workspace-epa/epatesting/all_resumes.csv
 nohup python3.6 /home/jgodoy/workspace-epa/epatesting/script.py "/home/jgodoy/workspace-epa/epatesting/config_example_local.ini" "/home/jgodoy/workspace-epa/epatesting/runs_example_local.ini" > /home/jgodoy/workspace-epa/epatesting/$newName &
 exit 0
