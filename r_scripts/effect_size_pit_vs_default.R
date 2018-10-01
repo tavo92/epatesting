f990e0e23a916d4c21b35dcdd409da19cf6354d9
JGodoy <j.godoy277@gmail.com>
2018-10-01 17:46:24 -0300

    csv table from R

---
+++ r_scripts/effect_size_pit_vs_default.R
@@ -0,0 +1,114 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+# test if there is at least one argument: if not, return an error
+if (length(args)< 1) {
+  stop("Input csv file argument must be supplied (report.csv)", call.=FALSE)
+}
+
+criterios <- list()
+i = 2
+while( i <= length(args))
+{
+    criterios <- c(criterios, args[i])
+    i = i + 1
+}
+csv_filename = args[1]
+
+# run the script
+stats = read.csv(csv_filename, header=TRUE, sep=",")
+
+subjects = unique(stats$SUBJ)
+tools = unique(stats$TOOL)
+budgets = unique(stats$BUD)
+
+measureA <- function(a,b){
+
+        if(length(a)==0 & length(b)==0){
+                return(0.5)
+        } else if(length(a)==0){
+                ## motivation is that we have no data for "a" but we do for "b".
+                ## maybe the process generating "a" always fail (eg out of memory)
+                return(0)
+        } else if(length(b)==0){
+                return(1)
+        } 
+
+        r = rank(c(a,b))
+        r1 = sum(r[seq_along(a)])
+
+        m = length(a)
+        n = length(b)
+        A = (r1/m - (m+1)/2)/n
+
+        return(A)
+
+}
+
+
+printHeader <- function()
+{
+	cat("subject")
+	i = 1
+	second_line = ""
+	while(i <= length(criterios))
+	{
+		curr_criterio = criterios[[i]]
+		i = i + 1
+		if (curr_criterio == "line_branch_exception")
+		{
+			next
+		}
+		# Repet for future table style
+		cat(",", curr_criterio)
+		cat(",", curr_criterio)
+		second_line = c(second_line, ", A12, p-value")
+	}
+	cat("\n")
+	cat("subject")
+	cat(second_line)
+	cat("\n")
+}
+
+calculateEffectSize <- function()
+{
+	for(subj in subjects)
+	{
+		cat(subj)
+		for (budget in budgets)
+		{
+			# LINE:BRANCH:EXCEPTION
+			default_rows  = subset(stats,SUBJ==subj & TOOL=='line_branch_exception' & BUD==budget)
+			default_errors = default_rows$PIMUT
+			
+			i = 1
+			while(i <= length(criterios))
+			{
+				criterio = criterios[[i]]
+				i = i + 1
+				if (criterio == "line_branch_exception")
+				{
+					next
+				}
+				rows  = subset(stats,SUBJ==subj & TOOL==criterio & BUD==budget)
+				errors = rows$PIMUT
+				
+				my_measureA = measureA(default_errors, errors)
+				cat(", ", my_measureA)
+				if (length(default_errors)==0)
+				{
+					cat("N/A")
+				} else
+				{
+					my_p_value = wilcox.test(default_errors, errors)$p.value
+					cat(", ", my_p_value)
+				}
+			}
+			cat("\n")
+		}
+	}
+}
+
+
+printHeader()
+calculateEffectSize()
