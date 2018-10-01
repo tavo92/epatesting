dc6faf1dc5c003c80847517924dbe6795a9b6ca8
JGodoy <j.godoy277@gmail.com>
2018-10-01 17:45:00 -0300

    generate csv and tex summary table

---
+++ R_results.py
@@ -0,0 +1,104 @@
+import subprocess
+import utils
+import csv
+
+
+def generate_latex_table(r_results_file, output):
+    def get_rows_columns_size(r_results_file):
+        with open(r_results_file, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            return len(reader.fieldnames)
+    i = 0
+    rows_size = get_rows_columns_size(r_results_file)
+    tabular_desc = "|"
+    while i < rows_size:
+        tabular_desc += "l|"
+        i += 1
+    declaration_init = "% Please add the following required packages to your document preamble:\n% \\usepackage{multirow}\n"
+    declaration_init += "\\begin{table}[]\n"
+    declaration_init += "\\caption{Insert here description}\label{summary-table}\n"
+    declaration_init += "\\begin{tabular}{" + "{}".format(tabular_desc)+"}"
+    declaration_init += "\n\\hline"
+    header = ""
+    subheader = ""
+    with open(r_results_file, "r") as results:
+        lines = []
+        i = 0
+        for line in results:
+            line = line.replace("_", "\_")
+            if i == 0:
+                firstLine = line.split(",")
+                i += 1
+                continue
+            if i == 1:
+                secondLine = line.split(",")
+                j = 0
+                while j < len(firstLine):
+                    curr_first_line_header = firstLine[j].strip()
+                    curr_second_line_header = secondLine[j].strip()
+                    if curr_second_line_header == curr_first_line_header:
+                        header += "\multirow{2}{*}"
+                        header += "{}".format("{"+curr_second_line_header+"}")
+                        subheader += " & "
+                        j += 1
+                        continue
+                    elif j+1 < len(firstLine) and firstLine[j].strip() == firstLine[j+1].strip():
+                        header += " & \multicolumn{2}{l|}"
+                        header += "{}".format("{"+firstLine[j+1].strip()+"}")
+                    subheader += curr_second_line_header
+                    if not j == len(firstLine)-1:
+                        subheader += " & "
+                    j += 1
+                header += " \\\\ \\cline{2-"+"{}".format(rows_size)+"}"
+                subheader += "\\\\ \\hline"
+            else:
+                curr_line = line.replace(",", " &")
+                curr_line = curr_line.replace("\n", "")
+                curr_line += " \\\\ \\hline"
+                lines.append(curr_line)
+            i += 1
+
+    content = ""
+    for __ in lines:
+        content += __ + "\n"
+    declaration_end = "{}\n{}".format("\end{tabular}", "\end{table}")
+    
+    table = declaration_init
+    table += header + "\n"
+    table += subheader + "\n"
+    table += content
+    table += declaration_end
+    print(table)
+    utils.save_file(output, table)
+
+
+def generate_r_results(r_executable_path, script_r_path, file_path, criterios_list, output):
+    def get_all_criterios_in_file(file_path):
+        criterios = []
+        with open(file_path, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if not row['TOOL'] in criterios:
+                    criterios.append(row['TOOL'])
+        return criterios
+
+    criterios = ""
+    if criterios_list[0] == "ALL":
+        criterios_list = get_all_criterios_in_file(file_path)
+        
+    i = 0
+    while i < len(criterios_list):
+        criterios += "\"{}\" ".format(criterios_list[i])
+        i += 1
+    command = "\"{}\" \"{}\" \"{}\" {} > {}".format(r_executable_path, script_r_path, file_path, criterios, output)
+    utils.print_command(command)
+    subprocess.check_output(command, shell=True)
+
+if __name__ == '__main__':
+    #parser = argparse.ArgumentParser()
+    #parser.add_argument("all_resumes_csv", help="all_resumes.csv")
+    #args = parser.parse_args()
+    generate_r_results("C:\\Program Files\\R\\R-3.5.0\\bin\\Rscript.exe", "C:\\Users\\JGodoy\\workspace-epa\\epatesting\\r_scripts\\effect_size_pit_vs_default.R", "C:\\Users\\JGodoy\\Desktop\\Results_analysis_generations\\Final_results\\SinAsserts\\all_resumes.csv",
+                  ["evosuite_epaalone", "epaadjacentedges", "epatransition_epaexception"], "temp")
+    generate_latex_table("C:\\Users\\JGodoy\\workspace-epa\\epatesting\\r_scripts\\out.txt", "C:\\Users\\JGodoy\\workspace-epa\\epatesting\\r_scripts\\out2.tex")
+    print("Done!")
